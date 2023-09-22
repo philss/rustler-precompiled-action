a4b602ebcfb5a336cfb3cbc06a85e70c855aa4c3
Philip Sampaio <philip.sampaio@gmail.com>
2023-09-22 17:47:54 -0300

    Support building with a variant name (#5)
    * Support building with a variant name
    
    This is part of https://github.com/philss/rustler_precompiled/pull/61
    
    * Add the "features" option and improve logging
    
    * Change the `features` input to `cargo-args`
    
    The idea is to have more flexibility, enabling more arguments to be
    passed.
    
    * Add a flag to enable debug mode compilation
    
    * Fix term and docs

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Usage: ./build.sh "$(pwd)" "$(pwd)" "x86_64-unknown-linux-gnu" 2.17 true
+# Usage: ./build.sh "$(pwd)" "$(pwd)" "x86_64-unknown-linux-gnu" 2.17 true "--features a,b --no-default-features"
 #
 # It composes the "build" command and executes it at the end.
 #
 # This script is also responsible for selecting the correct NIF version
 # using an ENV var or a cargo feature, depending on Rustler version.
+#
+# Set the env var "DRY_RUN" to "true" in order to see the command that
+# is going to build the project, instead of building it.
 
 initial_dir=$1
 project_dir=$2
 target_arch=$3
 nif_version=$4
 use_cross=${5:-"false"}
+cargo_args=${6:-""}
+
 logging=$(mktemp)
 
 # Version 0.29 is where RUSTLER_NIF_VERSION env var was deprecated.
@@ -87,9 +92,14 @@ cd "$project_dir"
 
 cargo_features=$(cargo tree -e features --depth 1 -i rustler -f "{p};{f}" --prefix none | head -n 1)
 
+echo "Cargo features: $cargo_features" >> $logging
+
 rustler_version=$(rustler_version "$cargo_features")
 rustler_nif_versions=$(rustler_nif_versions "$cargo_features")
 highest_nif_version=$(echo "$rustler_nif_versions" | head -n1)
+
+echo "Rustler version: $rustler_version" >> $logging
+echo "Highest NIF version: $highest_nif_version" >> $logging
 
 nif_version=$(echo -n "$nif_version" | tr '.' '_')
 desired_feature="nif_version_$nif_version"
@@ -100,46 +110,84 @@ if [ "$tool" = "cross" ]; then
   check_cross_config "$rustler_version"
 fi
 
-args="build --release --target=$target_arch"
+args="build --target=$target_arch"
+
+# A scape hatch to improve build times for when a release is not the intention.
+if [ "$RUSTLER_PRECOMPILED_DEBUG_MODE" != "true" ]; then
+  args="$args --release"
+fi
+
+# This is going to add arbritary args to the command, if the user provide it.
+if [ "$cargo_args" != "" ]; then
+  args="$args $cargo_args"
+fi
 
 echo "Rustler version: $rustler_version"
 echo "NIF version: $nif_version"
+echo "Desired feature for NIF version: $desired_feature"
 echo "Tool: $tool"
 
 # We try to modify args in case the desired feature is not the highest
-# activated, and the Rustler version is at least v0.29.0.
+# activated by default, and the Rustler version is at least v0.29.0.
 #
-# In case Rustler v0.30 or above is in use and the desired NIF version feature
-# could not be activated, we log an error and exit.
+# In case Rustler v0.30 or above is in use, and the desired NIF version
+# feature could not be activated, we log an error and exit.
 if [ "$highest_nif_version" != "$desired_feature" ]; then
+  echo "Highest NIF version is not equal to desired feature. We need to find a compatible NIF version feature..." >> $logging
+
   if [[ "$rustler_version" > "$rustler_features_since" ]] || [[ "$rustler_version" == "$rustler_features_since" ]]; then
+    echo "Rustler version is equal or above $rustler_features_since" >> $logging
+
     # So we test if the desired feature appears when we active it in the project.
-    cargo_features=$(cargo tree -e features --depth 1 -i rustler -f "{p};{f}" --prefix none -F "$desired_feature" 2>/dev/null | head -n 1)
+    cargo_features=$(cargo tree -e features --depth 1 -i rustler -f "{p};{f}" --prefix none -F "$desired_feature" 2>>"$logging" | head -n 1)
+
+    echo "Rustler features when desired feature is activated in this project: $cargo_features" >> $logging
 
     rustler_nif_versions=$(rustler_nif_versions "$cargo_features")
     highest_nif_version=$(echo "$rustler_nif_versions" | head -n1)
 
+    echo "Rustler NIF features related to versions: $rustler_nif_versions" >> $logging
+    echo "Highest Rustler NIF version from features: $highest_nif_version" >> $logging
+
     if [[ "$highest_nif_version" == "$desired_feature" ]]; then
+      echo "Successfully found feature to activate: $desired_feature" >> $logging
+
       args="$args --features $desired_feature"
     else
       if [[ "$rustler_version" > "$rustler_features_required_since" ]] || [[ "$rustler_version" == "$rustler_features_required_since" ]]; then
+        echo "Rustler version is equal or above $rustler_features_required_since, and it's not possible to activate the feature \"$desired_feature\" for the NIF version." >> $logging
+        echo "$logging"
         echo "::error file=Cargo.toml,line=1::The desired feature \"$desired_feature\" is not equal to the highest NIF version that is active: \"$highest_nif_version\""
         echo "::error file=Cargo.toml,line=1::Missing setup of NIF features that is required since Rustler $rustler_features_required_since. Please read the precompilation guide: https://hexdocs.pm/rustler_precompiled/precompilation_guide.html#additional-configuration-before-build"
         exit 1
       fi
     fi
+  else
+    echo "Rustler version is older than the one that uses features to activate NIF versions. So we ignore it for now." >> $logging
   fi
 fi
 
 echo "Arguments: $args"
 echo
 echo "Logs:"
+echo
 echo "$(cat $logging)"
+echo
 
-## Finally executes the command
-echo "Building..."
-eval "$tool" "$args"
+if [ "$DRY_RUN" = "true" ]; then
+  echo "Only printing command to run:"
+  echo
+  echo "$tool $args"
+  echo
+else
+  echo "Building..."
+  echo
+  # Eval is OK here :)
+  eval "$tool" "$args"
+  echo
+fi
+
 echo "Done."
-
 echo "Going back to original dir: $initial_dir"
+
 cd "$initial_dir"
